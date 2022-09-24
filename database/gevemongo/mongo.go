bb125987496f24bcbb676936c02898f9f72f2ad9
Denys Hromniuk <denys.hromniuk@gmail.com>
2022-09-24 12:36:16 +0200

    Selected optimal direction for gevemongo configuration

--- database/gevemongo/mongo.go
+++ database/gevemongo/mongo.go
@@ -1,6 +1,8 @@
 package gevemongo
 
 import (
+	"context"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -11,9 +13,43 @@ type mongoClient struct {
 
 func (c *mongoClient) database(name string, opts ...*options.DatabaseOptions) iDatabase {
 	return nil
+type singleResult struct {
+	*mongo.SingleResult
+}
+
+func (s *singleResult) decode(v interface{}) error {
+	return s.SingleResult.Decode(v)
+}
+
+type collection struct {
+	*mongo.Collection
+}
+
+func (c *collection) findOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) iSingleResult {
+	return &singleResult{c.Collection.FindOne(ctx, filter, opts...)}
+}
+
+type database struct {
+	*mongo.Database
+}
+
+func (d *database) collection(name string, opts ...*options.CollectionOptions) iCollection {
+	return &collection{d.Database.Collection(name, opts...)}
+}
+
+type client struct {
+	*mongo.Client
+}
+
+func (c *client) database(name string, opts ...*options.DatabaseOptions) iDatabase {
+	return &database{c.Client.Database(name, opts...)}
 }
 
 // MongoClient wraps a mongo.Client.
 func MongoClient(c *mongo.Client) iClient {
+<<<<<<< HEAD
 	return &mongoClient{c}
+=======
+	return &client{c}
+>>>>>>> 87bb281 (Selected optimal direction for gevemongo configuration)
 }
